a14616e7daf454af2eb60bc9b30dd560c0cf9166
alavyaa <bajpaialavya@gmail.com>
2026-05-23 15:02:51 +0530

    more note

--- oops.py
+++ oops.py
@@ -115,36 +115,85 @@ Modification done  with respect to object will not affect the class and other ob
 Reason: class are not depending on objects 
 
 
+States/  Property
+The data or information stored inside a class or an object is known as property of the class/object.
+OR
+The variable or functionalities storing inside the class are called states.
+
+There are are two types of states:-
+1. Generic State/ Class State/ Static State
+2. Object State/ Specific State
+
+~ these are the properties which will be common for every object.
+
+1. Generic State
+the properties which are common for all the objects are termed as generic state.
+
+Example:
+class School:
+    sname = "CGC"
+    loc = "Chandigarh"
+    principal = "Aditya"
+    time = "12:30 to 4:30"
+st1 = School()
+st2 = School()
+print(School.sname, School.loc, School.principal, School.time)
+print(st1.sname, st1.loc, st1.principal, st1.time)
+print(st2.sname, st2.loc, st2.principal, st2.time)
+
+2. Object state or Specific state
+The properties which will we create outside the class after the class creation is called as specific or object state.
+
+Example:
+
+class School:
+    sname = "DAV"
+    loc = "Bangalore"
+    principal = "Joseph Vijay"
+    time = "9:00am to 12:00pm"
+st1 = School()
+st1.name = "Shaurya"
+st1.id = 23
+st1.age = 20
+st1.bg = "AB+"
+
+st2 = School()
+st2.name = "Raj"
+st2.id = 24
+st2.age = 18
+st2.bg = "O+"
+print(st1.name, st1.id, st1.age, st1.bg, st1.sname, st1.loc, st1.principal, st1.time)
+print(st2.name, st2.id, st2.age, st2.bg, st2.sname, st2.loc, st2.principal, st2.time)
+
+Difference Betweek method and function.
+
+Functions which we declare inside a class are known as methods.
+If we declare the function is known as functions.
+
+Function:
+def add():
+    print("Hi")
+    
+Method:
+class Demo:
+    def show():
+        print("hi")
+        
+show() is a method.
+Constructor/ __init__ / initialisation
+
+Constructor is a special method .
+It runs automatically when an object is created.
+It is used to initialise the members of the object.
+No need of calling the method, by default it will execute when we create an object.
+self is the mandatory argument to pass for the __init__ method .
+We can pass arguments in the object creation only if there is __init__ method present inside the class.
+__init__ is the constructor method in python.
+
+For __init__ method, passing self is mandatory to store the address of the object. 
 
 '''
 
-class Bank:
-    bname = "SBI"
-    location = "Bangalore"
-    Manager = "Aditya"
 
-cus1 = Bank()
-cus2 = Bank()
 
-print(Bank.bname, Bank.location, Bank.Manager)
-print(cus1.bname, cus1.location, cus1.Manager)
-print(cus2.bname, cus2.location, cus2.Manager)
-
-Bank.location = "Chandigarh"         
-
-print(Bank.bname, Bank.location, Bank.Manager)
-print(cus1.bname, cus1.location, cus1.Manager)
-print(cus2.bname, cus2.location, cus2.Manager)
-
-cus1.location = "Jharkhand"
-
-print(Bank.bname, Bank.location, Bank.Manager)
-print(cus1.bname, cus1.location, cus1.Manager)
-print(cus2.bname, cus2.location, cus2.Manager)
-
-cus2.location = "Mumbai"
-
-print(Bank.bname, Bank.location, Bank.Manager)
-print(cus1.bname, cus1.location, cus1.Manager)
-print(cus2.bname, cus2.location, cus2.Manager)
 
