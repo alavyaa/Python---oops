3c6a2fbd7352425f5e9bf97de3975b80b1ec10a9
alavyaa <bajpaialavya@gmail.com>
2026-06-04 12:32:28 +0530

    lambda function

--- oops.py
+++ oops.py
@@ -1249,10 +1249,22 @@ obj3.emp_id()
 obj3.project()
 
 
+lambda function:
+it is a function which is anonymous in nature it is a function without name which is used to declare in 1 line
+we have to make use of lambda keyword to create a lambda function
+we can pass arguments according to the requirements
 
+an anonymous function which helps in creating single line function
 
+Syntax:
+lambda argument : expression
 
+b = lambda a : a**2 
+print(b(10))
+print((lambda a : a**3)(10))
+
+b = lambda a ,b: a*b 
+print(b(10, 20))
+print((lambda a : a**3)(10))
 
 '''
-
-
