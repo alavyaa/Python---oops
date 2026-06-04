8ebe861de6098bb231cb5f954898e26ad4e831c1
alavyaa <bajpaialavya@gmail.com>
2026-06-04 15:09:43 +0530

    map function

---
+++ map().py
@@ -0,0 +1,17 @@
+'''
+function map()
+it is a function which is used to perform same set of operation for each and every value present inside the collection  
+Syntax:
+
+map(function, collection)
+
+map(a, lambda x : x*x)
+
+a = [10, 20, 30, 40, 50]
+b = map(lambda x : x*x, a)
+print(list(b))
+
+'''
+a = ["Alavya", "Shaurya", "Vashu", "Aman"]
+
+
