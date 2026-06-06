476c585d3563fae562a255841609c565173e78c9
alavyaa <bajpaialavya@gmail.com>
2026-06-06 09:49:11 +0530

    comprehension

---
+++ comprehension.py
@@ -0,0 +1,14 @@
+'''
+Comprehension
+The phenomenon of creating a new collection by using less number of instructions
+It can be used for only mutable collection
+
+There are 3 types of comprehension
+1. List Comprehension
+2. Set Comprihension
+3. Dictionary Comprehension
+
+
+
+
+'''
