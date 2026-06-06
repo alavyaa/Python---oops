e2e2ef555a73971e951ad3f24aad47bca427c29f
alavyaa <bajpaialavya@gmail.com>
2026-06-06 11:40:48 +0530

    list comprehension

--- comprehension.py
+++ comprehension.py
@@ -8,7 +8,20 @@ There are 3 types of comprehension
 2. Set Comprihension
 3. Dictionary Comprehension
 
+1. List Comprehension
+the phenomenon of creating a new list by using less number of instruction
+to perform list comprehension we have 3 syntaxes
 
+Syntax:
+
+var = [val of var in collection if condition]
+
+var = [TSB if condition else FSB for var in collection]
+
+var = [var1, var2 for var1 in collection for var2 in collection]
+
+
+~ wap to create a list consisting of 1 to 10 int value [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 '''
