00117f44360a9387aa619e6f34395fed2c6a5432
alavyaa <bajpaialavya@gmail.com>
2026-06-08 10:40:06 +0530

    write function

--- file_handling.py
+++ file_handling.py
@@ -1,2 +1,17 @@
+'''
+in write mode we havee 2 methods
+write()
+writelines()
+Syntax: 
+f.writelines(iterables/collections)
+
+
+'''
+
 f = open("Sample.txt", "w")
-f.write("This is file handling")
+
+f.writelines(["Hello World\n", "2. MY bestfriend is the best gadha\n", "3. he is the best annoyer\n", "4. he is the best monkey\n"])
+
+f.close()
+
+#f.writelines()
