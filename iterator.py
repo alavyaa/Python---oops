7ff2480e40d81cd52f4997cfe9cf00a368acc596
alavyaa <bajpaialavya@gmail.com>
2026-06-09 10:29:47 +0530

    iterator

---
+++ iterator.py
@@ -0,0 +1,20 @@
+'''
+Iterator
+it is a process of traversing through sequence getting the value one by one 
+the function is used to perform iteration 
+for loop completely depends on iterator so that it has an inbuilt iterator
+to perform iterator it consista of two methods
+1. iter()
+2. next()
+
+
+
+
+
+'''
+a = [1, 2, 30, 40]
+it = iter(a)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
