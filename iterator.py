b9c6ed95ecd39fd8290ffdf2093f83ca37ecb498
alavyaa <bajpaialavya@gmail.com>
2026-06-09 12:36:57 +0530

    inter(0 and next()

--- iterator.py
+++ iterator.py
@@ -7,7 +7,18 @@ to perform iterator it consista of two methods
 1. iter()
 2. next()
 
+iter()
+it is a function which is used to make the control to get pointed to the initial node address
+Syntax:
+var = iter(iterables)
 
+iterables: multi value data (list, tuple, range, dictionary, set, strings)
+
+next()
+it is a function which is used to get the values one by one
+once you accessed all the objects inside the iterables that time it will throw StopIteration error
+Syntax:
+next(iterator())
 
 
 
@@ -18,3 +29,21 @@ print(next(it))
 print(next(it))
 print(next(it))
 print(next(it))
+
+
+class Count:
+    def __init__(self, start, stop):
+        self.current = start
+        self.stop = stop
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.current >= self.stop:
+            raise StopIteration
+        value = self.current
+        self.current += 1
+        return value
+
+obj = Count(1, 45)
+
+print(obj.__next__())
