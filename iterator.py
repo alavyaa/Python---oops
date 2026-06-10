29469115f40a756fc25c2730bdf2f5bb443febfb
alavyaa <bajpaialavya@gmail.com>
2026-06-10 10:11:39 +0530

    spelling

--- iterator.py
+++ iterator.py
@@ -3,7 +3,7 @@ Iterator
 it is a process of traversing through sequence getting the value one by one 
 the function is used to perform iteration 
 for loop completely depends on iterator so that it has an inbuilt iterator
-to perform iterator it consista of two methods
+to perform iterator it consists of two methods
 1. iter()
 2. next()
 
