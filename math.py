b370a18cbef51ec4883333e4f5da9e374d699b68
alavyaa <bajpaialavya@gmail.com>
2026-06-11 10:47:50 +0530

    math module

---
+++ math.py
@@ -0,0 +1,43 @@
+'''
+math
+
+import math before 
+we have 60+ functions in math module
+
+math.ceil()
+it rounds up the float value
+
+math.floor()
+it floors the float value always the befoe decimal value is the answer
+
+math.factorial()
+it calculates factorial
+
+math.pow()
+gives the a ** b 
+
+math.sqrt()
+gives square root
+always gives float value
+
+math.isqrt()
+gives the floor value
+
+math.gcd()
+gives greatest common divisor
+
+math.pi
+gives value of pi
+
+math.sin()
+gives float always
+
+math.log()
+gives log value with base e
+
+math.log10()
+gives log value with base 10
+
+'''
+import math
+print(math.pi)
