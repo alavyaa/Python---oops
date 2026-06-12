b5bd7b4b1e1e0b63ea7fe5140b3dc9e93c7e1528
alavyaa <bajpaialavya@gmail.com>
2026-06-12 10:43:48 +0530

    little example

--- decorator.py
+++ decorator.py
@@ -1,4 +1,44 @@
 '''
-decorator
+Decorator
 
+
+Syntax:
+def <decorator_name> (function_name):
+    def wrapper(*args, **kwargs):
+        pre task
+        function_name(*args, **kwargs):
+            ost wrapper
+    return wrapper
+    
+@<decorator_name>
+def main():
+    print("Task")
+main()
+
+Example:
+def hih(alu):
+    def wrapper():
+        print('This is the start of decorator.')
+        alu()
+        print('This is the end of decorator')
+    return wrapper
+@hih                                                 ~ first tareeka which we use always
+def greet():
+    print("Good Morning")
+    
+x = hih(greet)                                       ~ second tareeka which is internally being done
+x()
 '''
+def hih(alu):
+    def wrapper():
+        print('This is the start of decorator.')
+        alu()
+        print('This is the end of decorator')
+    return wrapper
+@hih
+def greet():
+    print("Good Morning")
+
+
+
+
