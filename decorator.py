7553eaf94d808c5cd2356f85274ed89432a0701e
alavyaa <bajpaialavya@gmail.com>
2026-06-15 10:44:15 +0530

    decorator complete

--- decorator.py
+++ decorator.py
@@ -1,6 +1,7 @@
 '''
 Decorator
-
+Decorators are functions used to add extra features to an existing function. They are useful when we
+have common pre-task and post-task operations to perform.
 
 Syntax:
 def <decorator_name> (function_name):
@@ -30,8 +31,34 @@ x = hih(greet)                                       ~ second tareeka which is i
 x()
 
 Two types 
-1. Built-in
+1. Inbuilt
 2. User-defined
+
+Inbuilt Decorators
+These are decorators whose tasks are already predefined by the developers
+Examples
+• @classmethod
+• @staticmethod
+• @property
+• @abstractmethod
+
+User Defined Decorators
+These are decorators that get created based on the user requirement
+
+Syntax
+def decorator_name(func):
+ def inner(*args, **kwargs):
+ # pre-task
+ func(*args, **kwargs)
+ # post-task
+ return inner
+@decorator_name
+def func_name(args):
+ # SB (Statement Block)
+func_name(values)
+
+
+
 
 
 '''
