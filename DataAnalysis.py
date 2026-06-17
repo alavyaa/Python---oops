c30cf74bd2cf89be62741ddbc802c61bd36f4046
alavyaa <bajpaialavya@gmail.com>
2026-06-17 10:20:37 +0530

    create data analysis

---
+++ DataAnalysis.py
@@ -0,0 +1,34 @@
+'''
+Data
+collection of information 
+it can be structured or unstructured 
+a person consumes 2GB data on average for like yt insta login shopping 
+
+Phase of Data Analysis
+
+as per data analyst only 5 phases are important
+while for an ml engineer and an ai developer all 6 phases are important
+
+1. Data Extraction
+    done by Data engineer from different sources
+    30% cleaning done here
+
+2. Data Cleaning
+    to remove or to add missing data and duplicate data
+    90% cleaning done here
+
+3. Feature Engineering
+    feature in data analysis means columns
+    to add and remove columns 
+
+4. Data Analyze and Visualise
+    visualising and all from matplotlib and seaborn
+    
+5. Buiseness Intelligence
+    working on different domains 
+    this talks with the owner
+    
+6. Machine Learning
+    heart of data
+    we train model in this
+'''
