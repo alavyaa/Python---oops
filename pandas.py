f03f33ddfaf729e8ac6cba62dc69e3130d3584bb
alavyaa <bajpaialavya@gmail.com>
2026-06-20 11:39:27 +0530

    Series

--- pandas.py
+++ pandas.py
@@ -8,5 +8,15 @@ Perprocessing and data cleaning
 Efficient for data analysis
 Integration with other libraries
 
+There are two Types of Data Structure
+1. Series
+2. Dataframe
+
+Series:
+It is a pandas data structure which is a 1 dimensional labeled array, it can hold any data
+
+Syntax to create a series:
+import pandas as pd
+var = pd.Series(array)
 
 '''
