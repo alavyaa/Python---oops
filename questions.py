cb965e2d6daf12076bc849058bd7cbae046d8c47
alavyaa <bajpaialavya@gmail.com>
2026-06-24 11:43:02 +0530

    kuhu

--- questions.py
+++ questions.py
@@ -17,4 +17,35 @@ df[(df['fare'] >= 20) & (df['fare'] <= 40)]
 4.
 # data where age is missing (Age = NaN)
 df[(df['age'].isnull())]
+
+5.
+# had sbsp>0
+df[(df['sibsp'] > 0)]
+
+6.
+# survived and paid more than median fare
+df[(df['survived'] == 1) & (df['fare'] > df['fare'].median())]
+
+7.
+# age < 18
+df[df['age'] < 18]
+
+8.
+# female and survived
+df[(df['sex'] == 'female') & (df['survived'] == 1)]
+
+9.
+#either in first or second class
+df[(df['pclass'] == 1) | (df['pclass'] == 2)]
+
+10.
+# less than 18 and survived
+df[(df['age'] < 18) & (df['survived'] == 1)]
+
+11.
+# females traveled in firstclass
+df[(df['sex'] == 'female') & (df['pclass'] == 1)]
+
+12.
+
 '''
